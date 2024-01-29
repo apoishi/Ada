78e8c1fd0521d9bcb6e2339020015eec2f6a242b
Ariane Padilha <ariane.padilha@outlook.com.br>
2024-01-29 20:56:31 -0300

    BaseTask add

---
+++ src/main/java/br/com/ada/pooii/domain/BaseTask.java
@@ -0,0 +1,12 @@
+package br.com.ada.pooii.domain;
+
+import java.util.Date;
+
+public class BaseTask {
+    private int id;
+    private String title;
+    private String description;
+    private Date deadline;
+
+}
+
